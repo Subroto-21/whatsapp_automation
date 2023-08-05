7becf3c04aaf52f7c0ef2984269e001a29b26ed8
Subroto Dinanath Naik <subrotonaik@gmail.com>
2023-08-05 17:06:38 +0530

    Add files via upload

--- whatsapp_automation.py
+++ whatsapp_automation.py
@@ -7,7 +7,7 @@ from time import sleep
 import pandas as pd
 import os
 from pathlib import Path
-import urllib.parse
+
 
 df=pd.read_json('Contact.json')
 
@@ -22,13 +22,19 @@ driver = webdriver.Edge(options = options)
 
 driver.get("https://web.whatsapp.com/")
 
-os.system("notepad input.txt")
 
-f=open("input.txt","r")
+share=df.loc[0,"msg"]
 
-share=urllib.parse.quote(f.read())
+df=pd.json_normalize(df['data'])
 
-input("Press enter after writng the message to send")
+wait = WebDriverWait(driver, 100)
+
+try:
+    wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')))
+    print("Logged in successfully.")
+except:
+    print("Login failed or took too long.")
+
 
 for index,row in df.iterrows():
     try:
@@ -37,7 +43,7 @@ for index,row in df.iterrows():
         driver.get(url)
 
         try:
-            click_btn =WebDriverWait(driver,10).until(
+            click_btn =WebDriverWait(driver,35).until(
                 EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span'))
             )
         except Exception as e:
@@ -50,5 +56,4 @@ for index,row in df.iterrows():
     except Exception as e:
         print('Failed to send message to ' + row["username"])
 driver.quit()
-f.close()
 print("The script executed successfully.")
